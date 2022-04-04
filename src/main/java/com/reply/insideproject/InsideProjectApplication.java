edc080ff038e885e57357704f33262ea6441f9cf
Sgarro <BydMrEC3tZpXVpzJhAYg>
2022-04-04 10:51:42 +0200

    init project

---
+++ src/main/java/com/reply/insideproject/InsideProjectApplication.java
@@ -0,0 +1,13 @@
+package com.reply.insideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsideProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reply/insideproject/InsideProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reply.insideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsideProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
